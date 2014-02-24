30f07208e3504c778695c6b155460e3d525c3a6b
Curtis Cali <botkilla2k12@gmail.com>
2014-02-24 18:47:41 -0500

    did more prototyping for ImageViewerWindow class

--- Medical Imaging System/src/ImageViewerWindow.java
+++ Medical Imaging System/src/ImageViewerWindow.java
@@ -3,4 +3,9 @@ import javax.swing.JFrame;
 
 public class ImageViewerWindow extends JFrame {
 	private ImagePanel imagePanel;
+	private ImageViewerMenuBar menuBar;
+	private NotificationPanel notifications;
+	private Settings settings;
+	private BrowseCommand browse;
+
 }
