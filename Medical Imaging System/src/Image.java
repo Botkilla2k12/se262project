29297589d9c519cd3fa311ad17264f40835f38ff
xdmds <xdmds200092@gmail.com>
2014-02-24 13:39:45 -0500

    Created Image.java

---
+++ Medical Imaging System/src/Image.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+import java.awt.image.*;
+
+/**
+ * @author derek
+ *
+ */
+public class Image {
+	private String fileName;
+	/**
+	 * 
+	 */
+	public Image(String fileName) {
+		// TODO Auto-generated constructor stub
+		this.fileName = fileName;
+	}
+
+	public String toString() {
+		return fileName;
+	}
+}
