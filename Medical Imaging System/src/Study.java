d300be79ac977e517e324db5cae5ed178de3e56f
xdmds <xdmds200092@gmail.com>
2014-02-24 13:28:22 -0500

    Created Study.java

---
+++ Medical Imaging System/src/Study.java
@@ -0,0 +1,27 @@
+import java.util.Observable;
+import java.util.Observer;
+import java.util.ArrayList;
+
+/**
+ * @author derek
+ *
+ */
+public class Study extends Observable {
+	private ArrayList<Image> Images;
+	
+	/**
+	 * 
+	 */
+	public Study() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
