2cbdcb851b45d169f32cb2e423710dd0e542da9d
Curtis Cali <botkilla2k12@gmail.com>
2014-02-24 17:23:26 -0500

    added ImagePanel and ImageViewerWindow classes.

---
+++ Medical Imaging System/src/ImagePanel.java
@@ -0,0 +1,12 @@
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JPanel;
+
+
+public class ImagePanel extends JPanel implements Observer {
+	@Override
+	public void update(Observable subject, Object data) {
+		// TODO Auto-generated method stub
+	}
+}
---
+++ Medical Imaging System/src/ImageViewerWindow.java
@@ -0,0 +1,6 @@
+import javax.swing.JFrame;
+
+
+public class ImageViewerWindow extends JFrame {
+	private ImagePanel imagePanel;
+}
