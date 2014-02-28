8740c1b809d7df40d99b87152833ebbe61e023c6
Luke <Luke@Luke-HP.wireless.rit.edu>
2014-02-28 18:04:25 -0500

    Added the basic structure of the menu bar.

--- Medical Imaging System/src/ImageViewerMenuBar.java
+++ Medical Imaging System/src/ImageViewerMenuBar.java
@@ -1,6 +1,20 @@
 import javax.swing.JMenuBar;
-
+import javax.swing.JMenu;
 
 public class ImageViewerMenuBar extends JMenuBar {
-
+	private JMenu fileMenu;
+	private JMenu editMenu;
+	private JMenu settingsMenu;
+	public ImageViewerMenuBar(){
+		this.fileMenu=new JMenu("File");
+		this.editMenu=new JMenu("Edit");
+		this.settingsMenu=new JMenu("Settings");
+		add(fileMenu);
+		add(editMenu);
+		add(settingsMenu);
+		
+		
+	}
+	
+	
 }
