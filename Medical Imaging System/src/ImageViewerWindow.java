c89908d3b667def937a3eaa16a47968ae5c2e560
Curtis <botkilla2k12@gmail.com>
2014-02-28 17:02:34 -0500

    Created NotificationPanel

--- Medical Imaging System/src/ImageViewerWindow.java
+++ Medical Imaging System/src/ImageViewerWindow.java
@@ -6,19 +6,21 @@ import javax.swing.JFrame;
 public class ImageViewerWindow extends JFrame {
 	private ImagePanel imagePanel;
 	private ImageViewerMenuBar menuBar;
-	//private NotificationPanel notifications;
+	private NotificationPanel notifications;
 	//private Settings settings;
 	//private BrowseCommand browse;
 
 	public ImageViewerWindow() {
 		this.menuBar = new ImageViewerMenuBar();
 		this.imagePanel = new ImagePanel();
+		this.notifications = new NotificationPanel();
 		
 		this.setJMenuBar(this.menuBar);
 		
 		this.setLayout(new BorderLayout());
 
-		this.add(imagePanel, BorderLayout.CENTER);
+		this.add(this.imagePanel, BorderLayout.CENTER);
+		this.add(this.notifications, BorderLayout.EAST);
 		
 		this.setVisible(true);
 		this.setSize(800, 600);
