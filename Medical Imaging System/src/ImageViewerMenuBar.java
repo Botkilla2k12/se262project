2ea99f606af9f5c49eb6e5ccd56f8fe8e7db13d3
Luke <Luke@Luke-HP.wireless.rit.edu>
2014-02-28 18:25:59 -0500

    Added simple functionality to the menubar

--- Medical Imaging System/src/ImageViewerMenuBar.java
+++ Medical Imaging System/src/ImageViewerMenuBar.java
@@ -1,16 +1,27 @@
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class ImageViewerMenuBar extends JMenuBar {
+	
 	private JMenu fileMenu;
 	private JMenu editMenu;
 	private JMenu settingsMenu;
-
+	//private JMenuItem open;
+	//private JMenuItem exit;
+	//private JMenuItem createStudy;
+	//private JMenuItem settingPlaceholder;
+	
+	
+	
 	public ImageViewerMenuBar(){
 		this.fileMenu=new JMenu("File");
+		fileMenu.add(new JMenuItem("Open..."));
+		fileMenu.add(new JMenuItem("Exit"));
 		this.editMenu=new JMenu("Edit");
+		editMenu.add(new JMenuItem("Create Study"));
 		this.settingsMenu=new JMenu("Settings");
-
+		settingsMenu.add(new JMenuItem("Settings stuff goes here..."));
 		add(fileMenu);
 		add(editMenu);
 		add(settingsMenu);
