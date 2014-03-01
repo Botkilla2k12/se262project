a953a045f2edae6db37e4012493de591fd3273cb
Curtis <botkilla2k12@gmail.com>
2014-02-28 19:08:50 -0500

    added prev and next button

--- Medical Imaging System/src/ImageViewerWindow.java
+++ Medical Imaging System/src/ImageViewerWindow.java
@@ -1,12 +1,15 @@
 import java.awt.BorderLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 
 public class ImageViewerWindow extends JFrame {
 	private ImagePanel imagePanel;
 	private ImageViewerMenuBar menuBar;
 	private NotificationPanel notifications;
+	private JButton prevButton, nextButton;
 	//private Settings settings;
 	//private BrowseCommand browse;
 
@@ -14,6 +17,8 @@ public class ImageViewerWindow extends JFrame {
 		this.menuBar = new ImageViewerMenuBar();
 		this.imagePanel = new ImagePanel();
 		this.notifications = new NotificationPanel();
+		this.prevButton = new JButton("Previous");
+		this.nextButton = new JButton("Next");
 		
 		this.setJMenuBar(this.menuBar);
 		
@@ -21,6 +26,14 @@ public class ImageViewerWindow extends JFrame {
 
 		this.add(this.imagePanel, BorderLayout.CENTER);
 		this.add(this.notifications, BorderLayout.EAST);
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new BorderLayout());
+		
+		buttonPanel.add(this.prevButton, BorderLayout.WEST);
+		buttonPanel.add(this.nextButton, BorderLayout.EAST);
+		
+		this.add(buttonPanel, BorderLayout.SOUTH);
 		
 		this.setVisible(true);
 		this.setSize(800, 600);
