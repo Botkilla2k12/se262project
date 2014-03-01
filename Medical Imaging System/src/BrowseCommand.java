f141a718cf981744e9d53082a2af7e069bdd662f
Matthew <mcw9612@rit.edu>
2014-02-28 22:23:51 -0500

    completed BrowseCommand

---
+++ Medical Imaging System/src/BrowseCommand.java
@@ -0,0 +1,86 @@
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+/**
+ * BrowseCommand.java
+ *
+ *@author Matthew Witte
+ *
+ *Version:
+ *$Id$
+ *
+ *Revisions:
+ *$Log$
+ */
+
+public class BrowseCommand {
+
+	private boolean direction; //true = right
+	private Study study;
+	private ArrayList<BufferedImage> images;
+	private DISPLAY_MODE_VALUE displayMode;
+	private int index = study.getIndex();
+	
+	public BrowseCommand(boolean direction, Study study) {
+		this.direction = direction;
+		this.study = study;
+		this.images = study.getImages();
+		this.displayMode = study.getStudySettings().getDisplayMode();
+	}
+	
+	public BufferedImage first() {
+		return images.get(0);
+	}
+	
+	public boolean isDone() {
+		if (displayMode == DISPLAY_MODE_VALUE.ONE_IMAGE) {
+			if (direction) { //moving to right by one image
+				return (index == images.size() - 1) ;
+			}
+			else { //moving to left by one image
+				return (index == 0);
+			}
+		}
+		else {
+			if (direction) { //moving to right by four images
+				return (index + 4 >= images.size());
+			}
+			else { //moving to left by four images
+				return (index - 4 <= 0);
+			}
+		}
+	}
+	
+	public BufferedImage currentItem() {
+		return images.get(index);
+	}
+	
+	public BufferedImage next() throws IndexOutOfBoundsException{
+		if (displayMode == DISPLAY_MODE_VALUE.ONE_IMAGE) {
+			if (direction && !isDone()) { //moving to right by one image
+				BufferedImage newImage = images.get(index + 1);
+				study.setIndex(index++);
+				return newImage;
+			}
+			else if (!direction && !isDone()){ //moving to left by one image
+				BufferedImage newImage = images.get(index - 1);
+				study.setIndex(index--);
+				return newImage;
+			}
+		}
+		else {
+			if (direction && !isDone()) { //moving to right by four images
+				BufferedImage newImage = images.get(index + 4);
+				study.setIndex(index += 4);
+				return newImage;
+			}
+			else if (!direction && !isDone()) { //moving to left by four images
+				BufferedImage newImage = images.get(index - 4);
+				study.setIndex(index -= 4);
+				return newImage;
+			}
+		}
+		throw new IndexOutOfBoundsException();
+	}
+	
+}
