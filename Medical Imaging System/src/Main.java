4aa2c1ea2097f6df93b504ee59d0c136d82fb544
Curtis <botkilla2k12@gmail.com>
2014-02-28 23:52:17 -0500

    updated main method,

--- Medical Imaging System/src/Main.java
+++ Medical Imaging System/src/Main.java
@@ -1,7 +1,46 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 
 public class Main {
 	public static void main(String[] args) {
-		Study studyModel = new Study();
-		ImageViewerWindow win = new ImageViewerWindow(studyModel);
+		try {
+			Scanner sc = new Scanner(new File("defaultstudy.cfg"));
+
+			String defaultPath = sc.nextLine();
+			SystemSettings settings = new SystemSettings();
+			
+			if(defaultPath.equals("null")) {
+				//prompt
+				int result = JOptionPane.showOptionDialog(
+					null,
+					"Would you like to select a default study?",
+					"Select default Study", 
+					JOptionPane.YES_NO_OPTION,
+					JOptionPane.QUESTION_MESSAGE,
+					null,
+					null,
+					null
+				);
+				
+				if(result == JOptionPane.YES_OPTION) {
+					//show file chooser and select directory
+				} else {
+					settings.setDefaultStudy(new Study(null));
+				}
+			} else {
+				settings.setDefaultStudy(new Study(new File(defaultPath)));
+			}
+			
+			ImageViewerWindow win = new ImageViewerWindow(
+				settings.getDefaultStudy()
+			);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
 	}
 }
--- Medical Imaging System/src/NotificationPanel.java
+++ Medical Imaging System/src/NotificationPanel.java
@@ -2,12 +2,17 @@ import java.awt.Color;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
 
 public class NotificationPanel extends JPanel implements Observer {
+	private BoxLayout layout;
+	
 	public NotificationPanel() {
 		this.setBackground(Color.WHITE);
+		
+		//this.layout = new BoxLayout(
 	}
 	
 	@Override
