3f7e4ddf3c98d473d983ed2426a7e91f505470b7
xdmds <xdmds200092@gmail.com>
2014-02-28 22:02:41 -0500

    re added previous changes that were lost

--- Medical Imaging System/src/StudySettings.java
+++ Medical Imaging System/src/StudySettings.java
@@ -5,17 +5,17 @@
  */
 public class StudySettings {
 	public static final int ONE_IMAGE = 1, FOUR_IMAGES = 4;
-	private int displayMode;
+	private DISPLAY_MODE_VALUE displayMode;
 	
 	public StudySettings() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public int getDisplayMode() {
+	public DISPLAY_MODE_VALUE getDisplayMode() {
 		return displayMode;
 	}
 	
-	public void setDisplayMode(int newDisplayMode) {
+	public void setDisplayMode(DISPLAY_MODE_VALUE newDisplayMode) {
 		this.displayMode = newDisplayMode;
 	}
 	
