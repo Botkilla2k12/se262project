742cc9769f7827cfa4198aa071c7dbcffacaf6be
Matthew <mcw9612@rit.edu>
2014-03-15 01:03:36 -0400

    added ShowACR class to open .acr files

---
+++ Medical Imaging System/src/ShowACR.java
@@ -0,0 +1,79 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import javax.imageio.stream.FileImageInputStream;
+
+public class ShowACR {
+    public static final int HEADER_OFFSET = 0x2000;
+    private BufferedImage image;
+
+    public ShowACR(File file) {
+
+		FileImageInputStream imageFile = null;
+		try {
+		    imageFile = new FileImageInputStream(file);
+		    imageFile.seek(HEADER_OFFSET);
+		}
+		catch (FileNotFoundException e) {
+		    System.err.print("Error opening file: ");
+		    System.err.println(e.getMessage());
+		    System.exit(2);
+		}
+		catch (IOException e) {
+		    System.err.print("IO error on file: ");
+		    System.err.println(e.getMessage());
+		    System.exit(2);
+		}
+	
+		int sliceWidth = 256;
+		int sliceHeight = 256;
+		    
+		BufferedImage sliceBuffer = 
+		    new BufferedImage( sliceWidth,sliceHeight,
+				       BufferedImage.TYPE_USHORT_GRAY );
+	
+		for ( int i = 0; i < sliceBuffer.getHeight(); i++ ) {
+		    for ( int j = 0; j < sliceBuffer.getWidth(); j++ ) {
+	
+				int pixelHigh = 0;
+				int pixelLow = 0;
+				int pixel;
+				
+				try {
+				    pixelHigh = imageFile.read();
+				    pixelLow = imageFile.read();
+				    pixel = pixelHigh << 4 | pixelLow >> 4;
+				    
+				    sliceBuffer.setRGB( j, i,
+						      pixel << 16 | pixel << 8 | pixel);
+		
+				}
+				catch (IOException e) {
+				    System.err.print("IO error readin byte: ");
+				    System.err.println(e.getMessage());
+				    System.exit(2);
+				}
+			
+			
+		    }
+		}
+		
+		this.image = sliceBuffer;
+		
+		/*JFrame win = new JFrame( "A Slice" );
+	        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+		win.getContentPane().add(new JLabel(new ImageIcon(sliceBuffer)));
+	        win.pack();
+	        win.setLocationRelativeTo(null);
+	        win.setVisible(true);*/
+    }
+    
+    public BufferedImage getImage() {
+    	return this.image;
+    }
+}
