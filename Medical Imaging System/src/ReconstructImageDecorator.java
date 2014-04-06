3e63edf3012a4332fb7d2aa7fe39d24264bc2dd1
Curtis Cali <botkilla2k12@gmail.com>
2014-04-06 12:36:24 -0400

    began work on decorators.

---
+++ Medical Imaging System/src/ReconstructImageDecorator.java
@@ -0,0 +1,16 @@
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+
+public abstract class ReconstructImageDecorator {
+	private BufferedImage image;
+	
+	public ReconstructImageDecorator(BufferedImage image) {
+		this.image = image;
+	}
+	
+	public void draw() {
+		Graphics2D g2d = (Graphics2D) this.image.getGraphics();
+		g2d.drawImage(this.image, 0, 0, null);
+	}
+}
